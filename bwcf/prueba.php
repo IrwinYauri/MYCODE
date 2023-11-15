05196ce30217ff56f8df1e85ca9b69cf9ccaa5a5
Irwin L. Yauri Orihuela <71727219+IrwinYauri@users.noreply.github.com>
2023-11-14 21:08:04 -0500

    Add files via upload

---
+++ bwcf/prueba.php
@@ -0,0 +1,24 @@
+<?php 
+
+	$maestro=setear("prueba.java");
+	$concursante=setear("ingreso.java");
+
+	if($maestro==$concursante)
+		echo "Correcto";
+	else
+		echo "Error";
+
+	function setear($archivo)
+	{
+		$fp = fopen($archivo, "r");
+		$cad="";
+		while (!feof($fp)){
+		    $linea = fgets($fp);
+		    $cad.=str_replace(' ', '', $linea);
+		}
+		fclose($fp);
+		return str_replace(' ', '', trim(preg_replace('/\s+/', ' ', str_replace(' ', '', $cad))));
+	}
+
+
+ ?>
